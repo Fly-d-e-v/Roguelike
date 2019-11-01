6852b5275c2d7bfa5a3a8ef6f133db1a822341f3
florianschut <mail@florianschut.nl>
2019-11-01 18:14:42 +0100

    Added GLM

--- Core/src/graphics/Renderer.cpp
+++ Core/src/graphics/Renderer.cpp
@@ -11,7 +11,7 @@ float vertices[] = {
 	 0.5f, -0.5f, 0.0f,		1.0f, 1.0f, 1.0f,   1.0f, 0.0f,   // bottom right
 	-0.5f, -0.5f, 0.0f,		1.0f, 1.0f, 1.0f,   0.0f, 0.0f,   // bottom left
 	-0.5f,  0.5f, 0.0f,		1.0f, 1.0f, 1.0f,   0.0f, 1.0f    // top left 
-};/
+};
 unsigned int indices[] = {
 	0, 1, 3,   // first triangle
 	1, 2, 3    // second triangle
