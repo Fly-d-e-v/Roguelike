e9e84be5106f272be3121305c7ddaf083d4d2dad
Timothy606 <timjlucassen@gmail.com>
2019-11-02 13:21:01 +0000

    Removed Auto

--- Core/src/input/devices/Keyboard.cpp
+++ Core/src/input/devices/Keyboard.cpp
@@ -10,25 +10,25 @@ void Keyboard::Init() {
     for (int i = 0; i <= GLFW_KEY_LAST; i++) {
         std::shared_ptr<KeyState> key = std::make_shared<KeyState>();
         key->m_ID = i;
-        m_Keys.insert(std::make_pair(i, key));
+        _Keys.insert(std::make_pair(i, key));
     }
 }
 
 void Keyboard::ProcessKeyEvent(int keyID, int keyState) {
 
-    auto keyPair = m_Keys.find(keyID);
-    if (keyPair != m_Keys.end()) {
+    auto keyPair = _Keys.find(keyID);
+    if (keyPair != _Keys.end()) {
 
         std::shared_ptr<KeyState> key = keyPair->second;
         switch (keyState) {
         case GLFW_PRESS:
-            key->m_State = EPressedState::Pressed;
+            key->_State = EPressedState::Pressed;
             break;
         case GLFW_REPEAT:
-            key->m_State = EPressedState::Held;
+            key->_State = EPressedState::Held;
             break;
         case GLFW_RELEASE:
-            key->m_State = EPressedState::Released;
+            key->_State = EPressedState::Released;
             break;
         }
     }
@@ -38,5 +38,5 @@ void Keyboard::ProcessKeyEvent(int keyID, int keyState) {
 }
 
 std::shared_ptr<KeyState> Keyboard::GetKeyState(int id) {
-    return m_Keys.find(id)->second;
+    return _Keys.find(id)->second;
 }
